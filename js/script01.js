c3d6a1134aef636575d7e597be14fb50aa9bb142
Roberto <roberto.iaconelli1@gmail.com>
2024-04-30 17:59:22 +0200

    TENETE

--- js/script01.js
+++ js/script01.js
@@ -3,4 +3,25 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+    loop:true,
+    autoplay : {
+        delay : 600,
+        enabled : true,
+    },
+    speed : 3000
   }); 
+
+  var swiper = new Swiper(".cards", {
+    slidesPerView: 4,
+    grid: {
+      rows: 1,
+    },
+    spaceBetween: 30,
+    loop : true,
+     autoplay : {
+        delay : 1000 ,
+        enabled : true,
+     },
+     
+  });
+
