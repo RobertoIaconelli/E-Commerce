e2fbc8a18b60020a850693050770d25c4b85b397
Roberto <roberto.iaconelli1@gmail.com>
2024-05-02 17:01:32 +0200

    linkActive

--- js/API.js
+++ js/API.js
@@ -1,9 +1,2 @@
 const URLAPI = "https://dummyjson.com/products";
 
-let urlPagina = window.location.pathname;
-console.log(urlPagina);
-let splittato = urlPagina.split("/").pop();
-console.log(splittato);
-
-let tutti = document.getElementsByClassName(".nav-link");
-console.log(tutti);
---
+++ js/linkActive.js
@@ -0,0 +1,10 @@
+let urlPagina = window.location.pathname;
+console.log(urlPagina);
+let splittato = urlPagina.split("/").pop();
+console.log(splittato);
+
+let tutti = document.getElementsByClassName(".nav-link");
+
+setTimeout(() => {
+    
+}, 5000);
