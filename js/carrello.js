5111b70e19f94003cb15b9bd988d82ca6258cdd8
Roberto <roberto.iaconelli1@gmail.com>
2024-05-07 15:33:28 +0200

    push

--- js/carrello.js
+++ js/carrello.js
@@ -3,15 +3,15 @@ let arrayCarrello = JSON.parse(localStorage.getItem("Carrello"));
 let listaPrezzi = document.querySelector(".listaPrezzi");
 let prezzoTotale = document.querySelector(".prezzoTotale2");
 
-function stampaCarrello(){
-    let somma = 0;
-    arrayCarrello.forEach(idProdotto=>{
-        fetch(`https://dummyjson.com/products/${idProdotto}`)
-        .then(response => {
-          return response.json()
-        })
-        .then(data => {
-            let oggettoCarrello = `<div class="row prodottoCarrello">
+function stampaCarrello() {
+  let somma = 0;
+  arrayCarrello.forEach((idProdotto) => {
+    fetch(`https://dummyjson.com/products/${idProdotto}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        let oggettoCarrello = `<div class="row prodottoCarrello">
             <div class="col-lg-3 ">
                 <div class="divImmagine">
                     <img src="${data.thumbnail}" class="immagineCarrello"/>
@@ -21,29 +21,56 @@ function stampaCarrello(){
                 <h1>${data.title}</h1>
                 <p>Quantità: ${data.stock}</p>
                 <p>Prezzo: ${data.price}€ </p>
-                <button data-id="${data.id} class="bg-danger btnDelete">Rimuovi</button>
+                <button data-id="${data.id}"  class="bg-danger btnDelete">Rimuovi</button>
                 </div>
-                </div>`
-                container.innerHTML+= oggettoCarrello;
-                
-                
-                let li = `<li>${data.title} - ${data.price}</li>`
-                listaPrezzi.innerHTML+= li;
-                somma += data.price;
-                console.log(somma);
-                prezzoTotale.innerHTML= somma;
-                console.log(prezzoTotale);
+                </div>`;
+        container.innerHTML += oggettoCarrello;
+
+        let li = `<li>${data.title} - ${data.price}</li>`;
+        listaPrezzi.innerHTML += li;
+        somma += data.price;
 
 
-                let btnDelete = document.querySelector(".btnDelete");
-                btnDelete.addEventListener("click", function(){
-                    console.log("ciao");
-                });
-            
-            })
+        prezzoTotale.innerHTML = somma;
+
+        eliminaProdotto();
+    });
+});
+}
+
+function eliminaProdotto() {
+    let btnDelete = document.querySelectorAll(".btnDelete");
+    btnDelete.forEach((btn) => {
+        btn.addEventListener("click", function () {
+            let bottoneId = this.getAttribute("data-id");
+            arrayCarrello = arrayCarrello.filter(
+                (idProdotto) => idProdotto !== bottoneId
+            );
+            localStorage.setItem("Carrello", JSON.stringify(arrayCarrello));
+            let cardProdotto = document.querySelector(".prodottoCarrello");
+            cardProdotto.remove();
+            aggiornamentoPrezzo();
+    });
+  });
+}
+
+function aggiornamentoPrezzo(){
+    let somma = 0;
+    prezzoTotale.innerHTML = "";
+    listaPrezzi.innerHTML="";
+    arrayCarrello.forEach(idProdotto=>{
+        fetch(`https://dummyjson.com/products/${idProdotto}`)
+        .then((response) => {
+            return response.json();
         })
-    }
-    
-    stampaCarrello();
-    
-    
+        .then(data=>{
+            somma+=data.price;
+            prezzoTotale.innerHTML = somma;
+            let li = `<li>${data.title} - ${data.price}</li>`;
+            listaPrezzi.innerHTML+= li;
+        })
+
+    })
+}
+
+stampaCarrello();
